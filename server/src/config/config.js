4feff0be3a05cff1dc6470aab094fc17b6d5cc50
Adin Had <adinhadzovic1993@gmail.com>
2022-02-02 20:01:31 +0100

    fixed new dotenv string for db connection

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -2,6 +2,5 @@ require('dotenv').config();
 
 module.exports = {
     port: process.env.PORT || "5000",
-    mongo: process.env.MONGO || 'mongodb://localhost:27017/',
-    secret: process.env.JWT_SECRET || 'mistral'
+    mongo: process.env.MONGO || 'mongodb://localhost:27017/'
 };
